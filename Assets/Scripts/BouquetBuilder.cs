17d49c136aaeb9d8583934b5384384b069a1d413
Andreea Straulescu <andreaela99@gmail.com>
2026-01-27 22:39:54 +0000

    After format pc

--- Assets/Scripts/BouquetBuilder.cs
+++ Assets/Scripts/BouquetBuilder.cs
@@ -5,10 +5,21 @@ public class BouquetBuilder : MonoBehaviour
 {
     public List<GameObject> placedFlowers = new List<GameObject>();
 
+    public static int placeFlowers = 0;
+     
     public void RegisterFlower(GameObject flower)
     {
         placedFlowers.Add(flower);
         flower.transform.SetParent(transform); // keep bouquet organised
         Debug.Log("Flowers placed: " + placedFlowers.Count);
+    }
+    public int GetFlowerCount()
+    {
+        return placedFlowers.Count;
+    }
+
+    public void ClearBouquet()
+    {
+        placedFlowers.Clear();
     }
 }
